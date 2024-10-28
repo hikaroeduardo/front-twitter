be0976d393c3a11ae548233ecd0a160478b51128
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-10-28 12:48:05 -0300

    feat: finalized navigation menu

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -6,6 +6,7 @@ import { NavItem } from "@/components/nav/nav-item";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { NavLogout } from "@/components/nav/nav-logout";
+import { NavMyProfile } from "@/components/nav/nav-myprofile";
 
 type Props = {
     children: ReactNode;
@@ -14,7 +15,7 @@ type Props = {
 export default function Layout({ children }: Props) {
     return (
         <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
-            <section className="flex lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
+            <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
                 <div className="flex-1 mt-6">
                     <Logo size={24} />
 
@@ -30,6 +31,7 @@ export default function Layout({ children }: Props) {
 
                 <div className="mb-6 flex flex-col gap-4">
                     <NavLogout />
+                    <NavMyProfile />
                 </div>
             </section>
 
